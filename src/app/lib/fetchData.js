e5887f4c0a88cb03887d078b0b4b92847789097f
pushkar1713 <pushkar1713@gmail.com>
2024-12-15 19:40:35 +0530

    deployment errors commit

--- src/app/lib/fetchData.js
+++ src/app/lib/fetchData.js
@@ -1,6 +1,6 @@
 "use server";
 
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 // Utility function to serialize Prisma data (handling Decimal and Date)
 function serializeData(data) {
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global;
 
-export { prisma };
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
